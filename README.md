a80860880d768f777fa7ff2fa35c98183983c4e2
beimg <beimgraben8@gmail.com>
2021-05-20 14:32:34 +0200

    Refactoring complete

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # c0-render
+Change the build parameters in src/main.c0
--- run.sh
+++ run.sh
@@ -2,8 +2,11 @@
 docker_c0() { docker run --rm --user=$(id -u):$(id -g) -v "$(pwd)":/code -w /code -it dbatunituebingen/c0:latest $@; }
 execc0() { docker_c0 ./$@; }
 cc0() { docker_c0 cc0 $@; }
-rm -rf ./build
+rm -rf ./build/*
 mkdir build
-cp ./src/*.c0 ./build/
+cp -r ./src/* ./build/
 cd build
 cc0 -d -x -o main main.c0
+cd ..
+cp build/out.png out/render_$(date +"%F").$(date +"%H-%M-%S").png
+rm -r build
