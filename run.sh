4754aa505885868235c6e661573feae75cd0c606
beimg <beimgraben8@gmail.com>
2021-05-21 15:22:41 +0200

    Prism Contructor (not working yet)

--- run.sh
+++ run.sh
@@ -2,11 +2,10 @@
 docker_c0() { docker run --rm --user=$(id -u):$(id -g) -v "$(pwd)":/code -w /code -it dbatunituebingen/c0:latest $@; }
 execc0() { docker_c0 ./$@; }
 cc0() { docker_c0 cc0 $@; }
-rm -rf ./build/*
 mkdir build
 cp -r ./src/* ./build/
 cd build
-cc0 -d -x -o main main.c0
+cc0 -d -x main.c0
 cd ..
 cp build/out.png out/render_$(date +"%F").$(date +"%H-%M-%S").png
 rm -r build
